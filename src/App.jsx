6333a665e24254fcb67c7b0e82af3fb7478f0c65
Jeferson <jefersonlozano07@gmail.com>
2024-09-28 22:54:13 -0500

    configuracion del state de cart para el manejo de item al agregarlo al car

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,39 @@ import { db } from './data/db'
 
 function App() {
 
-  const [data, setData]=useState(db)
-  
+  const [data, setData] = useState(db)
+  const [cart, setCart] = useState([]) 
+
+  function addToCart(item){
+    const itemExist = cart.findIndex(guitar => guitar.id === item.id)
+    if(itemExist >= 0){
+      const updatedCart = [...cart] // REALIZAMOS UNA COPIA DEL ARRAY DE CART PARA NO MUTARLO
+      updatedCart[itemExist].quantity++ // AGREGAMOS  1 UNIDAD AL PRODUCTO POR CADA CLICK
+      setCart(updatedCart) // 
+    }else{
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+  }
 
   return (
     <>
-      <Header />
+      <Header 
+        cart={cart}
+      />
         <main className="container-xl mt-5">
             <h2 className="text-center">Nuestra Colección</h2>
             <div className="row mt-5">
-              {data.map(()=>{
+              {data.map((guitar)=>{
                 return(
-                  <Guitar/>
+                  <Guitar 
+                    key={guitar.id}
+                    guitar = {guitar}
+                    cart = {cart}
+                    setCart={setCart}
+                    addToCart={addToCart}
+
+                  />
                 )
               })}
              
